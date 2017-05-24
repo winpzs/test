cb240f6ab293f1874841f63f3fa226e3fc79004d
winpzs <winpzs@gmail.com>
2017-05-24 14:01:58 +0800

    name of cmpx

--- src/__test__/test.ts
+++ src/__test__/test.ts
@@ -1,11 +1,26 @@
 
 import { expect } from 'chai';
 import 'mocha';
-import { lib } from '../index';
 
-describe('Hello function', () => {
-  it('should return hello world', () => {
-    var index = lib.inArray([1,2], 1);
+import cmpx from '../cmpx';
+
+describe('cmpx', () => {
+  it('inArray', () => {
+    var index = cmpx.inArray([1,2], 1);
     expect(index).to.equal(0);
+    index = cmpx.inArray([1,2], 2);
+    expect(index).to.equal(1);
+
+    var obj = {}
+    index = cmpx.inArray([1,2], function(item:any, index:number){
+      expect(obj === this).to.equal(true);
+      return item == 2;
+    }, obj);
+    expect(index).to.equal(1);
+
+    index = cmpx.inArray([1,2], function(item:any, index:number){
+      expect(obj != this).to.equal(true);
+      return item == 2;
+    });
   });
 });
--- src/cmpx.ts
+++ src/cmpx.ts
@@ -5,52 +5,65 @@ let stringEmpty = "",
     noop = function () { },
     slice = Array.prototype.slice;
 
-export const lib = {
-    stringEmpty: stringEmpty,
-    noop: noop,
-    hasOwnProp: function (obj:any, prop:string) {
+export default class cmpx {
+    static stringEmpty = stringEmpty;
+
+    static noop  = noop;
+
+    static hasOwnProp(obj:any, prop:string) {
         return core_hasOwn.call(obj, prop);
-    },
-    trace: function (e:Error) {
+    }
+
+    static trace(e:Error) {
         console.error && console.error(e.stack || e.message || e+'');
-    },
-    isType: function (typename:string, value:any) {
+    }
+
+    static isType(typename:string, value:any) {
         //typename:String, Array, Boolean, Object, RegExp, Date, Function,Number //兼容
         //typename:Null, Undefined,Arguments    //IE不兼容
         return toString.apply(value) === '[object ' + typename + ']';
-    },
-    isUndefined: function (obj:any) {
+    }
+
+    static isUndefined(obj:any) {
         ///<summary>是否定义</summary>
 
         return (typeof (obj) === "undefined" || obj === undefined);
-    },
-    isNull: function (obj:any) {
+    }
+
+    static isNull(obj:any) {
         ///<summary>是否Null</summary>
 
-        return (obj === null || this.isUndefined(obj));
-    },
-    isBoolean: function (obj:any) {
-        return this.isType("Boolean", obj);
-    },
-    isNullEmpty: function (s:any) {
-        return (this.isNull(s) || s === stringEmpty);
-    },
-    isFunction: function (fun:any) {
-        return this.isType("Function", fun);
-    },
-    isNumeric: function (n:any) {
-        //return this.isType("Number", n) && !isNaN(n) && isFinite(n);;
+        return (obj === null || cmpx.isUndefined(obj));
+    }
+
+    static isBoolean(obj:any) {
+        return cmpx.isType("Boolean", obj);
+    }
+
+    static isNullEmpty(s:any) {
+        return (cmpx.isNull(s) || s === stringEmpty);
+    }
+
+    static isFunction(fun:any) {
+        return cmpx.isType("Function", fun);
+    }
+
+    static isNumeric(n:any) {
+        //return cmpx.isType("Number", n) && !isNaN(n) && isFinite(n);;
         return !isNaN(parseFloat(n)) && isFinite(n);
-    },
-    isString: function (obj:any) {
-        return this.isType("String", obj);
-    },
-    isObject: function (obj:any) {
-        return !this.isNull(obj) && this.isType("Object", obj)
-            && !this.isElement(obj) && !this.isWindow(obj);//IE8以下isElement, isWindow认为Object
-    },
-    isPlainObject: function (obj) {
-        if (!this.isObject(obj)) {
+    }
+
+    static isString(obj:any) {
+        return cmpx.isType("String", obj);
+    }
+
+    static isObject(obj:any) {
+        return obj && cmpx.isType("Object", obj)
+            && !cmpx.isElement(obj) && !cmpx.isWindow(obj);//IE8以下isElement, isWindow认为Object
+    }
+
+    static isPlainObject(obj) {
+        if (!cmpx.isObject(obj)) {
             return false;
         }
         try {
@@ -71,25 +84,31 @@ export const lib = {
         for (key in obj) { }
 
         return key === undefined || core_hasOwn.call(obj, key);
-    },
-    isArray: function (value:any) {
-        return Array.isArray ? Array.isArray(value) : this.isType("Array", value);
-    },
-    isWindow: function (obj:any) { return !!(obj && obj == obj.window); },
-    isElement: function (obj:any) { var t = obj && (obj.ownerDocument || obj).documentElement; return t ? true : false; },
-    trim: function (str:string) {
+    }
+
+    static isArray(value:any) {
+        return Array.isArray ? Array.isArray(value) : cmpx.isType("Array", value);
+    }
+
+    static isWindow(obj:any) { return !!(obj && obj == obj.window); }
+
+    static isElement(obj:any) { var t = obj && (obj.ownerDocument || obj).documentElement; return t ? true : false; }
+
+    static trim(str:string) {
         return str.replace(/(^\s*)|(\s*$)|(^\u3000*)|(\u3000*$)|(^\ue4c6*)|(\ue4c6*$)/g, '');
-    },
-    replaceAll: function (s:string, str:string, repl:string, flags:string = "g") {
-        if (this.isNullEmpty(s) || this.isNullEmpty(str)) return s;
-        str = str.replace(/([^A-Za-z0-9])/g, "\\$1");
+    }
+
+    static replaceAll(s:string, str:string, repl:string, flags:string = "g") {
+        if (cmpx.isNullEmpty(s) || cmpx.isNullEmpty(str)) return s;
+        str = str.replace(/([^A-Za-z0-9 ])/g, "\\$1");
         s = s.replace(new RegExp(str, flags), repl);
         return s;
-    },
-    inArray:function(list:Array<any>, p: any, thisArg:any=null):number{
-        var isF = this.isFunction(p),
+    }
+
+    static inArray(list:Array<any>, p: any, thisArg:any=null):number{
+        var isF = cmpx.isFunction(p),
             index = -1;
-        this.each(list, (item, idx)=>{
+        cmpx.each(list, (item, idx)=>{
             var ok = isF ? p.call(thisArg, item, idx) : (item == p);
             if (ok){
                 index = idx;
@@ -97,11 +116,12 @@ export const lib = {
             }
         }, thisArg);
         return index;
-    },
-    each:function(list:Array<any>, fn:(item:any, idx:number)=>any, thisArg:any=null){
+    }
+
+    static each(list:Array<any>, fn:(item:any, idx:number)=>any, thisArg:any=null){
         var len = list.length;
         for (let i=0, len = list.length; i<len; i++){
             if (fn.call(thisArg, list[i], i) === false) break;
         }
     }
-};
+}
